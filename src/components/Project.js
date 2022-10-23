43907648902bad0cba3248cac9df27d99d5314ff
shubhojeet1 <103964132+shubhojeet1@users.noreply.github.com>
2022-10-23 13:18:45 +0530

    button

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Project = ({ item }) => {
   return (
     <div key={item.id} className='flex flex-col items-center text-center'>
-      <div className='mb-8'>
+      <div className='mb-8 h-4xl'>
         <img className='rounded-2xl' src={item.image} alt='' />
       </div>
       <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
@@ -12,6 +12,10 @@ const Project = ({ item }) => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit veniam
         obcaecati ipsam.
       </p>
+      <div className='flex flex-row items-center text-center'>
+        <button className='mt-16 mr-5 py-3 px-5' style={{"backgroundColor":"blue" ,"borderRadius":"10px","color":"#fff"}}><a href={item.source}>Source code</a></button>
+        <button className='mt-16 mr-5 py-3 px-5' style={{"backgroundColor":"red","borderRadius":"10px","color":"#fff"}}><a href={item.live}>Live demo</a></button>
+      </div>
     </div>
   );
 };
